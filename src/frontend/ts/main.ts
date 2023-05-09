202427f68a48070f3450002fa8ea18fa36b73998
Your Name <you@example.com>
2023-05-09 19:50:45 -0300

    clase 3 typescript herencia

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -6,13 +6,111 @@ function SayHello(){
 
 }
 
-var fun:any = SayHello;
-var numer:number = 21;
-var nombre: string = "matias";
+var fun: any = SayHello;
+
+interface ComportamientoAcceso{
+    accesoAmodificaciones(): boolean;
+    accesoAlLogin():boolean;
+}
+
+class Persona {
+    public nombre:string;
+    public dni: number;
+
+    constructor(nombre: string) {
+     
+        this.nombre = nombre;
+    }
+    
+    public saludar() {
+        console.log("hola soy " + this.nombre);
+    }
+}
+
+class Usuario extends Persona implements ComportamientoAcceso{
+    public userName: string;
+    constructor(usr: string, x: string) {
+        super(x);
+        this.userName = usr;
+        
+    }
+    public mostrar(): string{
+        return "usuario " + this.userName;
+    }
+    accesoAmodificaciones(): boolean{
+            return false;
+    }
+    accesoAlLogin(): boolean{
+        return true;
+    }
+    
+}
+class Administrador extends Persona implements ComportamientoAcceso{
+    accesoAmodificaciones(): boolean{
+        return true;
+    }
+    accesoAlLogin(): boolean{
+        return true;
+    }
+}
+class Main implements EventListenerObject {
+    users: Array<Usuario> = new Array();
+    
+    constructor() {
+        var usr1 = new Usuario("mramos", "Matias");
+        var usr2 = new Usuario("jlopez", "Juan");
+        this.users.push(usr1);
+        this.users.push(usr2);
+    }
+    handleEvent(event) {
+        if (event.target.id == "btnListar") {
+            for (var user of this.users){
+                console.log(user.mostrar())    
+            }
+            
+        } else {
+            this.users.push(new Usuario("usernuevo", "usernuevo"));    
+        }
+        
+    }
+
+
+}
+
+
+var usr = new Usuario("mramos","Matias");
+var adm = new Administrador("Jose");
+var per = new Persona("PErsona solo");
+
+
+var persona1: Persona;
+
+persona1 = usr;
+
+var comportamiento: ComportamientoAcceso = usr;
+
+
+
+var comp: ComportamientoAcceso = usr;
+
+usr.saludar();
+
+if (comp.accesoAlLogin()) {
+    
+}
+adm.saludar();
+
+
+
 
 
 window.addEventListener("load", () => {
     
-    fun();
-    SayHello();
+    var main: Main = new Main();
+    var btnListar: HTMLElement = document.getElementById("btnListar");
+    btnListar.addEventListener("click", main);
+
+    var btnAgregar: HTMLElement = document.getElementById("btnAgregar");
+    btnAgregar.addEventListener("click", main);
+    
 });
